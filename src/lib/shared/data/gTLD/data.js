25903c907cff01f0d956cdb8260eef6ea21c0d84
DarkDev <darkdev_brand@riseup.net>
2024-09-23 03:23:36 +0300

    refactoring structure

--- src/lib/shared/data/gTLD/data.js
+++ src/lib/shared/data/gTLD/data.js
@@ -2,7 +2,10 @@ const data = [
   {
     name: '.com',
     entity: 'commercial',
-    administrator: {
+     sponsor: {
+      title: '',
+      link: ''
+    }, administrator: {
       title: 'Verisign Inc.',
       link: 'https://en.wikipedia.org/wiki/Verisign'
     },
@@ -11,41 +14,47 @@ const data = [
       `Though originally intended for use by for-profit business entities, for a number of reasons it became the main TLD for domain names and is currently used by all types of entities including nonprofits, schools, and private individuals.`,
       `Domain name registrations may be successfully challenged if the holder cannot prove an outside relation justifying reservation of the name,[7] to prevent "squatting". It was originally administered by the United States Department of Defense.`,
     ],
-      idn: true,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: true,
       dnssec: true,
       sld: true,
       ipv6: true,
       id: 0,
       wikipedia: 'https://en.wikipedia.org/wiki/.com',
-      logo: '/images/domains/gTLD/com.svg',
+       logo: '/images/domains/gTLD/com.svg',
       promoImg: '/images/domains/gTLD/com.svg'
   },
     {
     name: '.org',
     entity: 'organization',
-    administrator: {
+     sponsor: {
+      title: '',
+      link: ''
+    }, administrator: {
       title: 'Public Interest Registry',
       link: 'https://en.wikipedia.org/wiki/Public_Interest_Registry'
     },
     notes: [
       `This is an open TLD; any person or entity is permitted to register.`,
       `Originally created as a miscellaneous category as stated in RFC 920 (October 1984) "...any other domains meeting the second level requirements," and clarified in RFC 1591 (March 1994)`,
-      `"This domain is intended as the miscellaneous TLD for organizations that didn't fit anywhere else. Some non-government organizations may fit here."[8][9] While mainly non-profits have used this domain, it was never restricted from miscellaneous use.`,
+      `"This domain is intended as the miscellaneous TLD for organizations that ddefaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn't fit anywhere else. Some non-government organizations may fit here."[8][9] While mainly non-profits have used this domain, it was never restricted from miscellaneous use.`,
 
     ],
-      idn: true,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: true,
       dnssec: true,
       sld: true,
       ipv6: true,
       id: 0,
       wikipedia: 'https://en.wikipedia.org/wiki/.org',
-      logo: '/images/domains/gTLD/org.svg',
+       logo: '/images/domains/gTLD/org.svg',
       promoImg: '/images/domains/gTLD/org.svg'
   },
     {
     name: '.net',
     entity: 'network',
-    administrator: {
+     sponsor: {
+      title: '',
+      link: ''
+    }, administrator: {
       title: 'Verisign Inc.',
       link: 'https://en.wikipedia.org/wiki/Verisign'
     },
@@ -53,19 +62,22 @@ const data = [
       `This is an open TLD; any person or entity is permitted to register.`,
       `According to RFC 1591 (March 1994) "This domain is intended to hold only the computers of network providers."`,
     ],
-      idn: true,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: true,
       dnssec: true,
       sld: true,
       ipv6: true,
       id: 0,
       wikipedia: 'https://en.wikipedia.org/wiki/.net',
-      logo: '/images/domains/gTLD/net.svg',
+       logo: '/images/domains/gTLD/net.svg',
       promoImg: '/images/domains/gTLD/net.svg'
   },
     {
     name: '.int',
     entity: 'international',
-    administrator: {
+     sponsor: {
+      title: '',
+      link: ''
+    }, administrator: {
       title: 'Internet Assigned Numbers Authority',
       link: 'https://en.wikipedia.org/wiki/Internet_Assigned_Numbers_Authority'
     },
@@ -73,19 +85,22 @@ const data = [
       `The .int TLD is strictly limited to organizations, offices, and programs endorsed by a treaty between two or more nations.`,
       `However, a few grandfathered domains do not meet these criteria.`,
     ],
-      idn: false,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: false,
       dnssec: true,
       sld: true,
       ipv6: true,
       id: 0,
       wikipedia: 'https://en.wikipedia.org/wiki/.int',
-      logo: '',
+       logo: '',
       promoImg: ''
   },
     {
     name: '.edu',
     entity: 'education',
-    administrator: {
+     sponsor: {
+      title: '',
+      link: ''
+    }, administrator: {
       title: 'Educause',
       link: 'https://en.wikipedia.org/wiki/Educause'
     },
@@ -94,51 +109,57 @@ const data = [
       ` In the U.S., its use was restricted in 2001 to post-secondary institutions accredited by an agency on the list of nationally recognized accrediting agencies.`,
       `This domain is therefore almost exclusively used by American colleges and universities. Some institutions, such as the Exploratorium, that do not meet the current registration criteria have grandfathered domain names.`,
     ],
-      idn: false,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: false,
       dnssec: true,
       sld: true,
       ipv6: true,
       id: 0,
       wikipedia: 'https://en.wikipedia.org/wiki/.edu',
-      logo: '/images/domains/gTLD/edu.svg',
+       logo: '/images/domains/gTLD/edu.svg',
       promoImg: '/images/domains/gTLD/edu.svg'
   },
   {
     name: '.gov',
     entity: 'U.S. national and state government agencies',
-    administrator: {
+     sponsor: {
+      title: '',
+      link: ''
+    }, administrator: {
       title: 'Cybersecurity and Infrastructure Security Agency',
       link: 'https://en.wikipedia.org/wiki/Cybersecurity_and_Infrastructure_Security_Agency'
     },
     notes: [
       `The .gov TLD is limited to United States governmental entities and agencies as well as qualifying state, county and local municipal government agencies, and tribal governments.`,
     ],
-      idn: false,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: false,
       dnssec: true,
       sld: true,
       ipv6: true,
       id: 0,
       wikipedia: 'https://en.wikipedia.org/wiki/.gov',
-      logo: '',
+       logo: '',
       promoImg: ''
   },
     {
     name: '.mil',
     entity: 'U.S. military',
-    administrator: {
+     sponsor: {
+      title: '',
+      link: ''
+    }, administrator: {
       title: 'United States Department of Defense',
       link: 'https://en.wikipedia.org/wiki/United_States_Department_of_Defense'
     },
     notes: [
       `The .mil TLD is limited to divisions, services and agencies of the United States Department of Defense.`,
     ],
-      idn: false,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: false,
       dnssec: true,
       sld: true,
       ipv6: true,
       id: 0,
       wikipedia: 'https://en.wikipedia.org/wiki/.mil',
-      logo: '',
+       logo: '',
       promoImg: ''
   },
 ]

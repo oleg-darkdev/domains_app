78b0a478976cb32e55b3b1bd801b0ea7d89d0d0a
DarkDev <darkdev_brand@riseup.net>
2024-09-23 03:23:31 +0300

    refactoring structure

--- src/lib/shared/data/ccTLD/dataR.js
+++ src/lib/shared/data/ccTLD/dataR.js
@@ -1,578 +1,117 @@
 const data = [
   {
-    name: '.r',
-    entity: '',
-    administrator: {
+    name: '.re',
+    entity: 'Réunion (France)',
+     sponsor: {
       title: '',
       link: ''
+    }, administrator: {
+      title: 'AFNIC',
+      link: 'https://en.wikipedia.org/wiki/Association_fran%C3%A7aise_pour_le_nommage_Internet_en_coop%C3%A9ration'
     },
     notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
+      `Restricted to individuals and companies in European Union, Switzerland, Norway, Iceland, and Liechtenstein.`,
+      `In recent years, the .re TLD has been increasingly used for real estate–related domains (RE = Real Estate), including place names (neighborhoods, cities, countries, etc.), real estate companies, developers/projects, property-related websites, and individual realtors and estate agents around the world. The .re domain has also been applied to insurance and reinsurance companies (e.g. swiss.re), those involved with reverse engineering, and as domain hacks for words ending in .re (for example, adventu.re or nearfutu.re), similar to contractions used with other TLDs (for example, websites such as bit.ly and delicio.us). Domains with the .re TLD can be registered with some of the largest online registrars such as Name.com, as well as most European domain registrars. `,
+      `Since 6 December 2011, the rules for registering French domains have shifted. The owner of a domain no longer needs to be based in France or Overseas France anymore, with all individuals, companies and organizations residing in the European Union able register domains of France or its possessions.`,
     ],
-      idn: 0,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: 0,
       dnssec: 0,
       sld: 0,
       ipv6: 0,
       id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
+      wikipedia: 'https://en.wikipedia.org/wiki/.re',
+       logo: '/images/domains/ccTLD/re.svg',
+      promoImg: '/images/domains/ccTLD/re.svg'
   },
   {
-    name: '.r',
-    entity: '',
-    administrator: {
+    name: '.ro',
+    entity: 'Romania',
+     sponsor: {
       title: '',
       link: ''
+    }, administrator: {
+      title: 'RNC',
+      link: 'https://en.wikipedia.org/wiki/.ro'
     },
     notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
+      `As of December 2007, about 250,000 domains were registered under the .ro domain.[2][3] In June 2008 there were around 6.8 million Google results for the .ro domain. In June 2012, there were 732,867 .ro domain names,[4] while in September 2017, there were 928,357.`,
     ],
-      idn: 0,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: 0,
       dnssec: 0,
       sld: 0,
       ipv6: 0,
       id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
+      wikipedia: 'https://en.wikipedia.org/wiki/.ro',
+       logo: '/images/domains/ccTLD/ro.svg',
+      promoImg: '/images/domains/ccTLD/ro.svg'
   },
   {
-    name: '.r',
-    entity: '',
-    administrator: {
+    name: '.rs',
+    entity: 'Serbia',
+     sponsor: {
       title: '',
+      link: ''
+    }, administrator: {
+      title: 'RNIDS',
       link: ''
     },
     notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
+      `The letters rs stand for Republika Srbija/Република Србија (Republic of Serbia). `,
+      `It is intended to be used with few restrictions, and it is widely used in Serbia. Because of English words ending with the letters "rs", this domain is also used in the construction of domain hacks,[2] and websites related to the Rust programming language, in which source code files typically end in .rs.`,
     ],
-      idn: 0,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: 0,
       dnssec: 0,
       sld: 0,
       ipv6: 0,
       id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
+      wikipedia: 'https://en.wikipedia.org/wiki/.rs',
+       logo: '/images/domains/ccTLD/rs.svg',
+      promoImg: '/images/domains/ccTLD/rs.svg'
   },
   {
-    name: '.r',
-    entity: '',
-    administrator: {
+    name: '.ru',
+    entity: 'Russia',
+     sponsor: {
       title: '',
       link: ''
+    }, administrator: {
+      title: 'Coordination Center for TLD RU',
+      link: 'https://en.wikipedia.org/wiki/Coordination_Center_for_TLD_RU'
     },
     notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
+      `See also .su, still in use, and .рф, for defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn. `,
+      `The creation of .ru in April 1994 was preceded by a historical agreement in 1993 known as "The order of RU top-level domain administration". According to the agreement, administrative and technical support responsibilities for the .ru domain were to be handed over to RIPN. In 2002, the administrative functions were handed over to CC for TLD RU.`,
     ],
-      idn: 0,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: 0,
       dnssec: 0,
       sld: 0,
       ipv6: 0,
       id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
+      wikipedia: 'https://en.wikipedia.org/wiki/.ru',
+       logo: '/images/domains/ccTLD/ru.svg',
+      promoImg: '/images/domains/ccTLD/ru.svg'
   },
   {
-    name: '.r',
-    entity: '',
-    administrator: {
+    name: '.rw',
+    entity: 'Rwanda',
+     sponsor: {
       title: '',
+      link: ''
+    }, administrator: {
+      title: 'RICTA',
       link: ''
     },
     notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
+      `Introduced	21 October 1996.`,
     ],
-      idn: 0,
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: 0,
       dnssec: 0,
       sld: 0,
       ipv6: 0,
       id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
-  },
-  {
-    name: '.r',
-    entity: '',
-    administrator: {
-      title: '',
-      link: ''
-    },
-    notes: [
-      ``,
-      ``,
-      ``,
-      ``,
-      ``,
-    ],
-      idn: 0,
-      dnssec: 0,
-      sld: 0,
-      ipv6: 0,
-      id: 0,
-      wikipedia: '',
-      logo: '/images/domains/icann/r.svg',
-      promoImg: '/images/domains/icann/r.svg'
+      wikipedia: 'https://en.wikipedia.org/wiki/.rw',
+       logo: '/images/domains/ccTLD/rw.svg',
+      promoImg: '/images/domains/ccTLD/rw.svg'
   },
 
 ]

26fdf926ea52884101a8db9529ef09d93b049966
DarkDev <darkdev_brand@riseup.net>
2024-09-23 03:23:19 +0300

    add new domains

---
+++ src/lib/shared/data/ccTLD/dataY.js
@@ -0,0 +1,52 @@
+const data = [
+  {
+    name: '.ye',
+    entity: 'Yemen',
+     sponsor: {
+      title: '',
+      link: ''
+    }, administrator: {
+      title: 'YemenNet',
+      link: 'https://en.wikipedia.org/wiki/YemenNet'
+    },
+    notes: [
+      `In 2015, when the Houthis took over the Yemeni capital, the rebel forces also took over the main internet provider and distributor of the .ye domain, TeleYemen, which remains under their control.[1] According to DomainTools, 1152 .ye domain names were registered as of 2018`,
+      `An internationalized country code top-level domain has been planned for the Republic of Yemen, intended for domain names in the local Arabic language. The string اليمن (al-Yaman) was registered and approved for this purpose in March 2011, but it was not activated and lower level domains were not granted at that time. `,
+    ],
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: 0,
+      dnssec: 0,
+      sld: 0,
+      ipv6: 0,
+      id: 0,
+      wikipedia: 'https://en.wikipedia.org/wiki/.ye',
+      logo: '/images/domains/ccTLD/ye.svg',
+      promoImg: '/images/domains/ccTLD/ye.svg'
+  },
+  {
+    name: '.yt',
+    entity: 'Mayotte',
+     sponsor: {
+      title: '',
+      link: ''
+    }, administrator: {
+      title: 'AFNIC',
+      link: 'https://en.wikipedia.org/wiki/Association_fran%C3%A7aise_pour_le_nommage_Internet_en_coop%C3%A9ration'
+    },
+    notes: [
+      `Restricted to individuals and companies in European Union, Switzerland, Norway, Iceland and Liechtenstein.[L] Also unofficially used for YouTube-related domains.`,
+      `.yt is the Internet country code top-level domain (ccTLD) for Mayotte, a part of the registry for France. The official registry address nic.yt redirects to the French registry site, AFNIC. Registrations, which had been suspended, resumed in December 2011.`,
+    ],
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: 0,
+      dnssec: 0,
+      sld: 0,
+      ipv6: 0,
+      id: 0,
+      wikipedia: 'https://en.wikipedia.org/wiki/.yt',
+       logo: '/images/domains/ccTLD/yt.svg',
+      promoImg: '/images/domains/ccTLD/yt.svg'
+  },
+
+]
+
+
+export default data;

5467739e4dda9b42a5f702012357ab83be25a3e3
DarkDev <darkdev_brand@riseup.net>
2024-09-27 02:52:59 +0300

    upd api

--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -1,4 +1,13 @@
-// import antISystems from './data/antISystems';
+import arpa from './data/arpa';
+import ccTLD from './data/ccTLD';
+import grTLD from './data/grTLD';
+import gTLD from './data/gTLD';
+import icann from './data/icann';
+import sTLD from './data/sTLD';
+import tTLD from './data/tTLD';
+import geoTLD from './data/geoTLD';
+import bTLD from './data/bTLD';
+// import arpa from './data/arpa';
 
-
-// export { };
+//  }
+export { ccTLD, arpa, geoTLD, bTLD, grTLD, gTLD, icann, sTLD, tTLD }

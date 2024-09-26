e0e3d6f85f01cb08ce98cfc905a413d51b36019e
DarkDev <darkdev_brand@riseup.net>
2024-09-27 02:54:51 +0300

    add content

---
+++ src/lib/shared/data/geoTLD/dataSouthAmerica.js
@@ -0,0 +1,55 @@
+const data = [
+  {
+    name: '.lat',
+    entity: 'Latin.',
+    sponsor: {
+      title: '',
+      link: '',
+    },
+    administrator: {
+      title: 'XYZ LLC',
+      link: '/',
+    },
+    notes: [`Latin America.`],
+    defaultPrice: 0,
+    introduced: '',
+    stats: '',
+    multiplier: 1,
+    idn: true,
+    dnssec: true,
+    sld: true,
+    ipv6: true,
+    id: 0,
+    wikipedia: 'https://en.wikipedia.org/wiki/.lat',
+    logo: '/images/domains/geoTLD/lat.svg',
+    promoImg: '/images/domains/geoTLD/lat.svg',
+  },
+  {
+    name: '.rio',
+    entity: 'Brazil.',
+    sponsor: {
+      title: '',
+      link: '',
+    },
+    administrator: {
+      title: '',
+      link: '/',
+    },
+    notes: [`Rio de Janeiro, Brazil.`],
+    defaultPrice: 0,
+    introduced: '',
+    stats: '',
+    multiplier: 1,
+    idn: true,
+    dnssec: true,
+    sld: true,
+    ipv6: true,
+    id: 0,
+    wikipedia: 'https://en.wikipedia.org/wiki/.rio',
+    logo: '/images/domains/geoTLD/rio.svg',
+    promoImg: '/images/domains/geoTLD/rio.svg',
+  },
+  
+]
+
+export default data

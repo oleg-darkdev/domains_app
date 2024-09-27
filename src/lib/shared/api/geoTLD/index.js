8d51866541ca124281d53c11ce21cf0f821d7519
DarkDev <darkdev_brand@riseup.net>
2024-09-27 19:54:04 +0300

    refactoring api, restructure geoTLD data

---
+++ src/lib/shared/api/geoTLD/index.js
@@ -0,0 +1,24 @@
+// geoTLD
+import dataAfrika from './dataAfrika'
+import dataAsia from './dataAsia'
+import dataEurope from './dataEurope'
+import dataFrance from './dataFrance'
+import dataGermany from './dataGermany'
+import dataNorthAmerica from './dataNorthAmerica'
+import dataOceania from './dataOceania'
+import dataSouthAmerica from './dataSouthAmerica'
+import dataSpain from './dataSpain'
+
+const data = [].concat(
+  dataAfrika,
+  dataAsia,
+  dataEurope,
+  dataFrance,
+  dataGermany,
+  dataNorthAmerica,
+  dataOceania,
+  dataSouthAmerica,
+  dataSpain,
+)
+
+export default data

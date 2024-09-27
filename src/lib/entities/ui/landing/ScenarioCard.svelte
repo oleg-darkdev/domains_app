ab17fcce02d07e0cb0b05a4cd044e8b0036a7f15
DarkDev <darkdev_brand@riseup.net>
2024-09-28 02:01:47 +0300

    init scenarios section

---
+++ src/lib/entities/ui/landing/ScenarioCard.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let scenario;
+</script>
+
+<div class="card collection">
+  <div class="collection-card-top-wrapper">
+    <img src="images/62bcd7364b6cf28e774f872b_opensea-logo.svg" loading="lazy" alt="Opensea" class="max-width-92px">
+    <h3 class="display-3 mg-top-32px">{scenario.title}</h3>
+    <p>Donec ullamcorper felis at massa nibh venenatis suspendisse urna condimentum commodo vel vitae massa id at ut eget dolor pretium volutpat donec porttitor et.</p>
+    <a href="https://opensea.io/" target="_blank" class="primary-button mg-top-20px w-button">Buy on Opensea</a>
+  </div>
+  <img src="images/62bcd736884c9fdab1601eef_open-sea-bottom-image.png" loading="lazy"  alt="" sizes="(max-width: 479px) 83vw, (max-width: 767px) 85vw, (max-width: 991px) 87vw, 84vw" class="mg-top-65px">
+</div>
+
+
+
+<style>
+
+</style>
---
+++ src/lib/widgets/ui/landing/Scenarios.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { ScenarioCard } from '$lib/entities';
+  import { scenarios } from '$lib/shared';
+</script>
+
+<section class="section">
+  <div class="container-default w-container">
+    <a name="scenarios"></a>
+
+    <div class="max-width-845px text-center">
+      <h1 class="rodamas-font small opacity-58">Collection</h1>
+      <h2 class="display-2">Browse the full collection on</h2>
+    </div>
+    <div class="w-layout-grid grid-collection-cards">
+      {#each scenarios as scenario}
+        <ScenarioCard {scenario}/>
+      {/each}
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>

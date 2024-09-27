ef86f93d2b3e2f72a36b83dfc89962231d67c2c7
DarkDev <darkdev_brand@riseup.net>
2024-09-27 20:00:46 +0300

    refactoring, add api points

---
+++ src/lib/shared/api/template/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    name: '',
+    entity: '',
+     sponsor: {
+      title: '',
+      link: ''
+    }, administrator: {
+      title: '',
+      link: ''
+    },
+    notes: [
+      ``,
+      ``,
+      ``,
+      ``,
+      ``,
+    ],
+      defaultPrice: 0, introduced: '', stats: '', multiplier: 1, idn: 0,
+      dnssec: 0,
+      sld: 0,
+      ipv6: 0,
+      id: 0,
+      wikipedia: '',
+       logo: '/images/domains/template/.svg',
+      promoImg: '/images/domains/template/.svg'
+  },
+]
+
+
+export default data;

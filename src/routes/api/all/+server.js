c05273079f0bc4879af281fda00b290e292a978f
DarkDev <darkdev_brand@riseup.net>
2024-09-27 20:01:29 +0300

    init

---
+++ src/routes/api/all/+server.js
@@ -0,0 +1,18 @@
+import { json } from '@sveltejs/kit';
+import {
+  apiBTLDall,
+  apiCCTLDallAll,
+  icannDataAll,
+  apiGeoTLDAll,
+} from '$lib/shared'
+
+const allDomains = [].concat(
+  apiBTLDall,
+  apiCCTLDallAll,
+  icannDataAll,
+  apiGeoTLDAll,
+);
+
+export async function GET(event) {
+	return json(allDomains)
+}

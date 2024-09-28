674396735da6269774883dbb31d4f6aeb8ec4966
DarkDev <darkdev_brand@riseup.net>
2024-09-28 03:13:26 +0300

    upd text

--- src/lib/widgets/ui/landing/Scenarios.svelte
+++ src/lib/widgets/ui/landing/Scenarios.svelte
@@ -8,8 +8,8 @@
     <a name="scenarios"></a>
 
     <div class="max-width-845px text-center">
-      <h1 class="rodamas-font small opacity-58">Collection</h1>
-      <h2 class="display-2">Browse the full collection on</h2>
+      <h1 class="rodamas-font small opacity-58">Scenarios</h1>
+      <h2 class="display-2">Just pick a scenario and play in app!</h2>
     </div>
     <div class="w-layout-grid grid-collection-cards">
       {#each scenarios as scenario}

4863c4c007afd809b29cdb7714a66da35982e3d0
DarkDev <darkdev_brand@riseup.net>
2024-09-28 20:25:13 +0300

    init 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,33 @@
+
+<script>
+
+</script>
+
+<section class="wf-section">
+  <div class="container-default w-container">
+    <div class="utility-page-wrap">
+      <div class="utility-page-content">
+        <img
+          src="/images/404.svg"
+          alt=""
+          class="bg-image _404"
+        />
+        <h1 style="font-size: 100px;" class="text-black z-20">
+          404! PAGE NOT FOUND
+        </h1>
+        <div
+          style=""
+          class="button-wrapper"
+        >
+          <a href="/" class="primary-button mg-top-32px w-button"
+            >Go back to homepage</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<style>
+
+</style>

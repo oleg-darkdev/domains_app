114e76422f33c286e04987fc239b615eda6d6e7a
DarkDev <darkdev_brand@riseup.net>
2024-09-28 03:13:05 +0300

    refactoring data structure

--- src/lib/entities/ui/landing/ScenarioCard.svelte
+++ src/lib/entities/ui/landing/ScenarioCard.svelte
@@ -1,15 +1,28 @@
 <script>
   export let scenario;
 </script>
-
-<div class="card collection">
+<!--
+   complexity: 'basic',
+  timeToPlay: 'basic',
+  wikipedia
+  domains length
+ -->
+<div class="card rounded-none collection">
   <div class="collection-card-top-wrapper">
-    <img src="images/62bcd7364b6cf28e774f872b_opensea-logo.svg" loading="lazy" alt="Opensea" class="max-width-92px">
+    <div class="flex justify-start">
+      <img src="{scenario.icon}" loading="lazy" alt="" class="h-14">
+    </div>
+
     <h3 class="display-3 mg-top-32px">{scenario.title}</h3>
-    <p>Donec ullamcorper felis at massa nibh venenatis suspendisse urna condimentum commodo vel vitae massa id at ut eget dolor pretium volutpat donec porttitor et.</p>
-    <a href="https://opensea.io/" target="_blank" class="primary-button mg-top-20px w-button">Buy on Opensea</a>
+    <p>
+      {scenario.shortDesc}
+    </p>
+    <a href="/app" target="_blank" class="primary-button mg-top-20px w-button">Run
+      <span style='font-family: Rodamas, sans-serif; font-size: 70px;' class='mx-4'>{scenario.abbr}</span>
+      scenario
+    </a>
   </div>
-  <img src="images/62bcd736884c9fdab1601eef_open-sea-bottom-image.png" loading="lazy"  alt="" sizes="(max-width: 479px) 83vw, (max-width: 767px) 85vw, (max-width: 991px) 87vw, 84vw" class="mg-top-65px">
+  <img src="{scenario.promoImg}" loading="lazy"  alt="" class="mg-top-65px">
 </div>
 
 

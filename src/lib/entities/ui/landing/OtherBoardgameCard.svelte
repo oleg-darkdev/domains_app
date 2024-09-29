f9e1500c312ea09cf391eafe42b1085b42ec7300
DarkDev <darkdev_brand@riseup.net>
2024-09-29 19:44:26 +0300

    init other boardgames components

---
+++ src/lib/entities/ui/landing/OtherBoardgameCard.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let boardgame;
+</script>
+
+<div
+    class="split-content full-height"
+  >
+    <a
+      href="{boardgame.link}"
+      target="_blank"
+      class="card social rounded-none w-inline-block"
+      ><div class="social-card-top-wrapper">
+        <img
+          src="{boardgame.icon}"
+          loading="lazy"
+          alt="{boardgame.title} icon."
+          class="card-social-icon"
+        />
+        <h3>{boardgame.title}</h3>
+      </div>
+      <p class="color-neutral-400">
+        {boardgame.shortDesc}
+      </p>
+      <div class="social-card-bottom-wrapper">
+        <h5>More info</h5>
+        <div class="button-arrow-wrapper partner">
+          <div class="icon-font right-up-arrow">�&nbsp;�</div>
+        </div>
+      </div></a
+    >
+  </div>
+
+
+  <style>
+
+  </style>
---
+++ src/lib/widgets/ui/landing/OtherBoardgames.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { OtherBoardgameCard } from '$lib/entities';
+  
+  export let otherBoardgames;
+</script>
+
+<section class="section social-media wf-section">
+  <div class="container-default w-container">
+    <div
+      class="divider contact-page neutral-700"
+    ></div>
+    <div class="max-w-6xl text-center">
+      <h1
+        class="rodamas-font small mg-bottom-0"
+      >
+        Other IT boardgames
+      </h1>
+      <h2
+        class="display-2 mg-bottom-48px mg-top-20px"
+      >
+        Lorem Ipsum is simply dummy text.
+      </h2>
+    </div>
+    <div class="w-layout-grid grid-3-columns">
+      {#each otherBoardgames as boardgame}
+        <OtherBoardgameCard {boardgame} />
+      {/each}
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>

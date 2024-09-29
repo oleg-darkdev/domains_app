afdd41908364fbd6742af60c15c1dd5563328c37
DarkDev <darkdev_brand@riseup.net>
2024-09-30 02:27:46 +0300

    alfa v.

---
+++ src/lib/entities/ui/landing/SmallTeamMemberCard.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let member;
+</script>
+
+
+<div
+  role="listitem"
+  class="w-dyn-item"
+>
+<a class="" href='{member.link}' target="_blank">
+  <div class="card rounded-none h-40">
+    <img class='h-8' src="{member.mainImg}" />
+  </div>
+  </a>
+</div>
+
+
+<style>
+
+</style>

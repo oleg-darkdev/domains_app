2b9fb6e904c1c200d1211296c639a7104732d0d8
DarkDev <darkdev_brand@riseup.net>
2024-09-29 19:43:49 +0300

    init faq components

---
+++ src/lib/entities/ui/landing/FaqCard.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { fade } from 'svelte/transition';
+
+  export let faq;
+</script>
+
+<div class="accordion-item">
+  <div class="accordion-header" on:click={()=> faq.showAnswer = !faq.showAnswer}>
+    <div class="close-icon-wrapper">
+      <div class="close-icon">
+        <div class="line left ease-linear transform  transition duration-200 {faq.showAnswer ? '-rotate-45': ''}"></div>
+        <div class="line right ease-linear transform  transition duration-200 {faq.showAnswer ? 'rotate-45': ''}"></div>
+      </div>
+    </div>
+    <h3 class="display-3 max-width-968px">
+      {faq.q}
+    </h3>
+  </div>
+
+  <div class="accordion-body">
+    {#if faq.showAnswer}
+      {#each faq.a as answer}
+        <p class="color-neutral-400 " transition:fade>
+          {answer}
+        </p>
+      {/each}
+    {/if}
+  </div>
+</div>
+
+
+
+<style>
+
+</style>
---
+++ src/lib/widgets/ui/landing/Faq.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { FaqCard } from '$lib/entities';
+  import { faqData } from '$lib/shared';
+</script>
+
+<section class="section faqs wf-section">
+  <div class="container-default w-container">
+    <div
+      class="divider contact-page neutral-700"
+    ></div>
+    <div class="max-w-4xl mx-auto text-center">
+      <h1
+        class="rodamas-font small"
+      >
+        FAQs
+      </h1>
+      <h2
+        class="display-2"
+      >
+        Frequently Asked Questions
+      </h2>
+      <p
+        class="mg-top-16px mg-bottom-48px"
+      >
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </p>
+    </div>
+    <div class="w-layout-grid grid-nft-accordion">
+      <div
+        class="accordion-item-wrapper"
+      >
+
+      {#each faqData as faq}
+        <FaqCard {faq}/>
+      {/each}
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>

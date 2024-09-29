f210b28d5abfdef32df84f2121ca34c62a1705d4
DarkDev <darkdev_brand@riseup.net>
2024-09-29 19:44:59 +0300

    alfa v.

---
+++ src/lib/entities/ui/landing/Contact.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+
+<section class="">
+  <div class="container-default w-container">
+
+    <a name="contact"></a>
+
+    <div class="footer-top">
+      <div class="split-content left-top-footer">
+        <a href="/" aria-current="page" class="header-logo-link footer w-nav-brand w--current">
+          <img src="/images/logo.svg" loading="lazy" alt="" class="lg:h-20 h-10">
+        </a>
+      </div>
+      <div class="split-content right-footer">
+        <div class="w-layout-grid header-social-wrapper footer">
+          <a href="https://discord.com" target="_blank" class="social-link-icon w-inline-block">
+            <div class="icon-font social-icon">�&nbsp;�</div>
+          </a>
+          <a href="https://rarible.com/" target="_blank" class="social-link-icon w-inline-block">
+            <div class="icon-font social-icon rarible">�&nbsp;�</div>
+          </a>
+          <a href="https://opensea.io/" target="_blank" class="social-link-icon w-inline-block">
+            <div class="icon-font social-icon open-sea">�&nbsp;�</div>
+          </a>
+        </div>
+        <a href="/" class="primary-button small w-button">Get in Touch!</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>

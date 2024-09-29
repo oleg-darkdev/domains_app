0f941a9fa819a8964256ac7ff4505f051ad2e578
DarkDev <darkdev_brand@riseup.net>
2024-09-29 19:43:29 +0300

    init articles routes

---
+++ src/routes/articles/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { ArticlesSection, HeaderLanding,  } from '$lib/widgets'
+  import {BigArticleCard, BigNewsletterCard } from '$lib/entities';
+  import {articlesList} from '$lib/shared/'
+</script>
+
+<HeaderLanding />
+
+<main>
+  <section class="section hero blog">
+    <div class="container-default">
+      <div class="hero-top-wrapper">
+        <h1
+          class="rodamas-font"
+        >
+          Blog
+        </h1>
+        <h1
+          class="max-width-720px mg-top-56px mg-bottom-48px"
+        >
+          Articles &amp; resources
+        </h1>
+      </div>
+
+      <BigArticleCard article={articlesList[0].articles[0]}/>
+    </div>
+  </section>
+
+
+  <BigNewsletterCard />
+
+  <ArticlesSection />
+</main>
+
+<!-- <Footer /> -->

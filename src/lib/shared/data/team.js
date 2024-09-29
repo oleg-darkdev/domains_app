15274014ceb6fb709a602f6917dbaf72f21ecd0e
DarkDev <darkdev_brand@riseup.net>
2024-09-30 02:27:56 +0300

    add content

--- src/lib/shared/data/team.js
+++ src/lib/shared/data/team.js
@@ -1,5 +1,20 @@
 const team = [
-
-];
+  {
+    mainImg: '/images/logos/wikipedia.svg',
+    fakeImg: '',
+    bio: '',
+    fullName: '',
+    link: 'https://en.wikipedia.org/wiki/Main_Page',
+    subTitle: '',
+  },
+  {
+    mainImg: '/images/logos/darkdev_turq.svg',
+    fakeImg: '',
+    bio: '',
+    fullName: '',
+    link: 'https://darkdev-foundation.vercel.app/',
+    subTitle: '',
+  },
+]
 
 export default team;

58af1a983907f0a3715b7e5f04fc003169ccb5ea
DarkDev <darkdev_brand@riseup.net>
2024-09-29 19:41:56 +0300

    init other scenarious component

---
+++ src/lib/entities/ui/app/OtherScenarioCardApp.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let scenario;
+</script>
+
+<a
+  href=""
+  target="_blank"
+  class="card partner rounded-none my-2 w-inline-block"
+  ><img
+    src="{scenario.icon}"
+    loading="lazy"
+    alt="{scenario.abbr}"
+    class="partner-image"
+  />
+  <div class="divider neutral-600 partner"></div>
+  <div class="partner-card-bottom-wrapper">
+    <h3>{scenario.title}</h3>
+    <div class="button-arrow-wrapper partner">
+      <div class="icon-font right-up-arrow">�&nbsp;�</div>
+    </div>
+  </div></a
+>
+
+
+<style>
+
+</style>
---
+++ src/lib/widgets/ui/app/OtherScenarios.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { OtherScenarioCardApp } from '$lib/entities';
+
+  export let scenarios;
+</script>
+
+<section class="section ">
+  <div class="container-default w-container">
+    <!-- blog-top-content -->
+    <div class="">
+      <div class="max-w-4xl">
+        <h1
+          class="rodamas-font small"
+        >
+          Other scenarios.
+        </h1>
+        <h2
+          class="display-2"
+        >
+          Discover the other scenarios.
+        </h2>
+      </div>
+      <div class="split-content">
+        <p
+          class="max-w-md"
+        >
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+      </div>
+    </div>
+    <div class="w-dyn-list">
+      <div role="list" class="grid-3-columns w-dyn-items">
+        {#each scenarios as scenario}
+          <OtherScenarioCardApp {scenario}/>
+        {/each}
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>

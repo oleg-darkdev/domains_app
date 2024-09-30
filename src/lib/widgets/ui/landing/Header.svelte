25f6a3facb2ec9ae6e45ea3a7facad0d925ebfa7
DarkDev <darkdev_brand@riseup.net>
2024-09-30 04:38:25 +0300

    add mobile menu

--- src/lib/widgets/ui/landing/Header.svelte
+++ src/lib/widgets/ui/landing/Header.svelte
@@ -1,9 +1,13 @@
 <script>
   import { navigation } from '$lib/shared';
+  import { fade } from 'svelte/transition';
+    import { ContactBlock } from '$lib/entities';
+
+  let showMenu = false;
 </script>
 
 <div role="banner" class="navbar w-nav">
-  <div class="container-default w-container">
+  <div class="container-default w-container flex flex-col">
     <div class="header-wrapper">
       <div class="split-content left-header">
         <a href="/" aria-current="page" class="header-logo-link w-nav-brand w--current">
@@ -29,15 +33,68 @@
         </div> -->
 
         <a href="/app" target="_blank" class="primary-button small hide-on-mobile w-button">App</a>
-        
-        <div class="menu-button w-nav-button">
+
+        <div on:click={()=> showMenu = !showMenu} class="menu-button w-nav-button {showMenu ? 'w--open': ''}">
           <div class="w-icon-nav-menu"></div>
         </div>
       </div>
     </div>
+      {#if showMenu}
+        <div class="h-screen w-full" transition:fade>
+        <nav role="navigation" class="mb-14 flex flex-col">
+          {#each navigation as {title, link}}
+            <a href="{link}" aria-current="page" class="header-nav-link w-nav-link w--current">{title}</a>
+          {/each}
+
+          <a href="/app" target="_blank" class="primary-button small w-button">App</a>
+        </nav>
+
+        <ContactBlock />
+        </div>
+      {/if}
   </div>
 </div>
 
 <style>
+.w-nav-button {
+  position: relative;
+  float: right;
+  padding: 18px;
+  font-size: 24px;
+  display: none;
+  cursor: pointer;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  tap-highlight-color: rgba(0, 0, 0, 0);
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.w-nav-button:focus {
+  outline: 0;
+}
+.w-nav-button.w--open {
+  background-color: #c8c8c8;
+  color: white;
+}
 
+
+@media screen and (max-width: 991px) {
+  .w-nav-menu {
+    display: none;
+  }
+  .w-nav-button {
+    display: block;
+  }
+}
+@media screen and (max-width: 767px) {
+  .w-nav[data-collapse="small"] .w-nav-button {
+    display: block;
+  }
+}
+@media screen and (max-width: 479px) {
+  .w-nav[data-collapse="tiny"] .w-nav-button {
+    display: block;
+  }
+}
 </style>

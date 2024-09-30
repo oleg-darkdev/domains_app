04c85300c0d832c2d21d36f6cb7f9e1586ae1ff6
DarkDev <darkdev_brand@riseup.net>
2024-09-30 04:39:29 +0300

    init social links for contact block

---
+++ src/lib/shared/data/socialLinks.js
@@ -0,0 +1,21 @@
+const socialLinks = [
+  {
+    icon: '◽️',
+    text: '',
+    link: '/',
+  },
+  {
+    icon: '◽️',
+    text: '',
+    link: '/',
+  },
+  {
+    icon: '◽️',
+    text: '',
+    link: '/',
+  },
+
+]
+
+
+export default socialLinks;

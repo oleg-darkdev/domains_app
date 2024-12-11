1d16dd20e355fe8e29ef5d471325d88c77435b82
Bear-software <linner_952@mail.ru>
2024-12-11 17:04:03 +0100

    fix layout

--- src/lib/entities/ui/landing/RoadmapCard.svelte
+++ src/lib/entities/ui/landing/RoadmapCard.svelte
@@ -10,13 +10,17 @@
 
   <ul class="mb-14 flex flex-col " role="list">
     {#each point.features as feature}
-      <li class='flex flex-row mb-4 content-center '>
+      <li class='mb-4 w-full'>
+        <div class="flex flex-row w-full">
         <div class=' mr-4 '>
           <!-- <img src="/images/list-item.svg" class='h-4 w-4' alt=""> -->
           <span class='h-4 w-4' style='font-family: "Clash Grotesk", sans-serif; font-weight: 700; font-size: 24px;'>+</span>
         </div>
 
-        {feature}
+        <p class='text-left '>
+          {feature}
+        </p>
+        </div>
       </li>
     {/each}
   </ul>
